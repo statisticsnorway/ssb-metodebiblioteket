e15cd11e9f2d7fcab96e2e27eb2caddd39aaf310
Susie Jentoft <coo@ssb.no>
2023-04-19 09:10:56 +0200

    Added update option. Moved creating csv to new file.

--- R/add_funcs.R
+++ R/add_funcs.R
@@ -1,9 +1,19 @@
 # Functions for adding new functiond
 
-add_func <- function(func, package, keyword = "r", url = NULL, github = F){
+#func ="validator"
+#package="validate"
+
+add_func <- function(func, package, keyword = "r", url = NULL, github = F, update=F){
   kat <- read.csv('katalogdata.csv', header = T, stringsAsFactors = FALSE)
   
-  if (check_func(func, package, kat) > 0) stop("Function already in library")
+  if (check_func(func, package, kat) > 0) {
+    if (!update) {
+      stop("Function already in library. Use parameter 'update=T' to override")
+    } else {
+      m <- which(package == kat$pack & func == kat$func)
+      kat <- kat[-m, ]
+    }
+  } 
   if (missing(url)){
     url <- get_url(func, package, github)
   }
@@ -67,41 +77,5 @@ get_name <- function(func, package){
   navn <- trimws(txt[3])
   navn
 }
-
-
-
-#### Add functions ####
-#
-
-
-# Kkontrollere funksjoner
-add_func('validator', 'validate', keyword = "r kontrollere")
-add_func('confront', 'validate', keyword = "r kontrollere")
-add_func('ThError', 'Kostra', keyword = "r kontrollere", github = T)
-add_func('Hb', 'Kostra', keyword = "r kontrollere", github = T)
-add_func('Quartile', 'Kostra', keyword = "r kontrollere", github = T)
-add_func('OutlierRegressionMicro', 'Kostra', keyword = "r kontrollere", github = T)
-add_func('Rank2NumVar', 'Kostra', keyword = "r kontrollere", github = T)
-add_func("Diff2NumVar", "Kostra", keyword = "r kontrollere", github = T)
-add_func("AggrSml2NumVar", "Kostra", keyword = "r kontrollere", github = T)
-
-#Imputere
-add_func('impute_proxy', 'simputation', keyword = "r imputere")
-add_func('impute_knn', 'simputation', keyword = "r imputere")
-add_func('lm', 'stats', keyword = "r imputere analyse")
-add_func('impute_pmm', 'simputation', keyword = "r imputere")
-add_func('impute_rhd', 'simputation', keyword = "r imputere")
-
-# Strukturere
-add_func("HierarchyCompute", "SSBtools", keyword = "r strukturere")
-
-# Konfidensialitet
-add_func("PLSrounding", "SmallCountRounding", keyword = "r konfidensialitet")
-add_func("ProtectKostra", "Kostra", keyword = "r konfidensialitet", github = T)
-add_func("ProtectTable", "easySdcTable", keyword = "r konfidensialitet", github = T)
-
-# Analyse
-
-
 
 
