42fe12d89e553227dda189cb6c4f01a6836acc94
Susie Jentoft <coo@ssb.no>
2023-04-21 15:28:33 +0200

    Added reexports file for functions to be tested.

---
+++ R/reexports.R
@@ -0,0 +1,206 @@
+
+#' @importFrom easySdcTable EasyData
+# @export
+easySdcTable::EasyData
+ 
+
+#' @importFrom CalibrateSSB CalibrateSSB
+# @export
+CalibrateSSB::CalibrateSSB
+
+
+#' @importFrom CalibrateSSB PanelEstimation
+# @export
+CalibrateSSB::PanelEstimation
+ 
+
+#' @importFrom validate validator
+# @export
+validate::validator
+
+
+#' @importFrom validate confront
+# @export
+validate::confront
+
+
+#' @importFrom Kostra ThError
+# @export
+Kostra::ThError
+
+
+#' @importFrom Kostra Hb
+# @export
+Kostra::Hb
+
+
+#' @importFrom Kostra Quartile
+# @export
+Kostra::Quartile
+
+
+#' @importFrom Kostra OutlierRegressionMicro
+# @export
+Kostra::OutlierRegressionMicro
+
+
+#' @importFrom Kostra Rank2NumVar
+# @export
+Kostra::Rank2NumVar
+
+
+#' @importFrom Kostra Diff2NumVar
+# @export
+Kostra::Diff2NumVar
+
+
+#' @importFrom Kostra AggrSml2NumVar
+# @export
+Kostra::AggrSml2NumVar
+
+
+#' @importFrom struktuR get_extremes
+# @export
+struktuR::get_extremes
+
+
+#' @importFrom simputation impute_proxy
+# @export
+simputation::impute_proxy
+
+
+#' @importFrom simputation impute_knn
+# @export
+simputation::impute_knn
+
+
+#' @importFrom stats lm
+# @export
+stats::lm
+
+
+#' @importFrom simputation impute_pmm
+# @export
+simputation::impute_pmm
+
+
+#' @importFrom simputation impute_rhd
+# @export
+simputation::impute_rhd
+
+
+#' @importFrom Kostra LmImpute
+# @export
+Kostra::LmImpute
+
+
+#' @importFrom SSBtools HierarchyCompute
+# @export
+SSBtools::HierarchyCompute
+
+
+#' @importFrom SmallCountRounding PLSrounding
+# @export
+SmallCountRounding::PLSrounding
+
+
+#' @importFrom SmallCountRounding PLSroundingPublish
+# @export
+SmallCountRounding::PLSroundingPublish
+
+
+#' @importFrom Kostra ProtectKostra
+# @export
+Kostra::ProtectKostra
+
+
+#' @importFrom easySdcTable ProtectTableData
+# @export
+easySdcTable::ProtectTableData
+
+
+#' @importFrom SdcForetakPerson SdcForetakPerson
+# @export
+SdcForetakPerson::SdcForetakPerson
+
+
+#' @importFrom GaussSuppression SuppressSmallCounts
+# @export
+GaussSuppression::SuppressSmallCounts
+
+
+#' @importFrom GaussSuppression SuppressDominantCells
+# @export
+GaussSuppression::SuppressDominantCells
+
+
+#' @importFrom GaussSuppression SuppressFewContributors
+# @export
+GaussSuppression::SuppressFewContributors
+
+
+#' @importFrom GaussSuppression SuppressKDisclosure
+# @export
+GaussSuppression::SuppressKDisclosure
+
+
+#' @importFrom GaussSuppression GaussSuppressDec
+# @export
+GaussSuppression::GaussSuppressDec
+
+
+#' @importFrom GaussSuppression GaussSuppressionFromData
+# @export
+GaussSuppression::GaussSuppressionFromData
+
+
+#' @importFrom GaussSuppression SuppressionFromDecimals
+# @export
+GaussSuppression::SuppressionFromDecimals
+
+
+#' @importFrom GISSB shortest_path_igraph
+# @export
+GISSB::shortest_path_igraph
+
+
+#' @importFrom GISSB shortest_path_cppRouting
+# @export
+GISSB::shortest_path_cppRouting
+
+
+#' @importFrom SSBpris CalcInd
+# @export
+SSBpris::CalcInd
+
+
+#' @importFrom SSBpris CalcIndS2
+# @export
+SSBpris::CalcIndS2
+
+
+#' @importFrom struktuR struktur_model
+# @export
+struktuR::struktur_model
+
+
+#' @importFrom CalibrateSSB CalibrateSSB
+# @export
+CalibrateSSB::CalibrateSSB
+
+
+#' @importFrom CalibrateSSB PanelEstimation
+# @export
+CalibrateSSB::PanelEstimation
+
+
+#' @importFrom SSBtools model_aggregate
+# @export
+SSBtools::model_aggregate
+
+
+#' @importFrom SSBtools quantile_weighted
+# @export
+SSBtools::quantile_weighted
+
+
