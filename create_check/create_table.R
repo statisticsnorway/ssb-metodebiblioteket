63faca40cd663ef3b951127056ab24a1d05772cf
Susie Jentoft <coo@ssb.no>
2023-04-24 15:17:48 +0200

    Lagt inn sesongjusterings funksjoner

--- create_check/create_table.R
+++ create_check/create_table.R
@@ -6,11 +6,13 @@ devtools::install_github("statisticsnorway/Kostra")
 devtools::install_github("statisticsnorway/SmallCountRounding")
 devtools::install_github("statisticsnorway/easySdcTable")
 devtools::install_github("statisticsnorway/SSBpris")
-devtools::install_github("statisticsnorway/pickmdl")
 devtools::install_github("statisticsnorway/SdcForetakPerson")
 install.packages("GISSB")
-install.packages("RJDemetra")
 install.packages("GaussSupression")
+
+# RJDemetra litt vanskelig å installere pga avhengighet på java versjon
+# devtools::install_github("statisticsnorway/pickmdl")
+# install.packages("RJDemetra")
 
 
 #### Add functions ####
@@ -55,12 +57,23 @@ add_func("GaussSuppressDec", "GaussSuppression", keyword = "r konfidensialitet u
 add_func("SuppressionFromDecimals", "GaussSuppression", keyword = "r konfidensialitet undertrykking")
 
 
-# Sesongjustering - missing html for functions in pickmdl. Can't install with Java
-#add_func("x13", "RJDemetra", keyword = "r sesongjustering")
-#add_func("x13_spec", "RJDemetra", keyword = "r sesongjustering")
-#add_func("x13_pickmdl", "pickmdl", keyword = "r sesongjustering", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd")
-#add_func("x13_automdl", "pickmdl", keyword = "r sesongjustering", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd")
-#add_func("x13_both", "pickmdl", keyword = "r sesongjustering", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_both.Rd")
+# Sesongjustering - missing html for functions in pickmdl. Can't install with Java so use manual
+
+add_func("x13", "RJDemetra", keyword = "r sesongjustering", url="https://rdrr.io/cran/RJDemetra/man/x13.html",
+         name = "Seasonal Adjustment with X13-ARIMA",
+         descrip = "Functions to estimate the seasonally adjusted series (sa) with the X13-ARIMA method. This is achieved by decomposing the time series (y) into the trend-cycle (t), the seasonal component (s) and the irregular component (i). The final seasonally adjusted series shall be free of seasonal and calendar-related movements. x13 returns a preformatted result while jx13 returns the Java objects resulting from the seasonal adjustment.")
+add_func("x13_spec", "RJDemetra", keyword = "r sesongjustering", url = "https://rdrr.io/cran/RJDemetra/man/x13_spec.html",
+        name = "X-13ARIMA model specification, SA/X13",
+        descrip = 'Function to create (and/or modify) a c("SA_spec", "X13") class object with the SA model specification for the X13 method. It can be done from a pre-defined "JDemetra+" model specification (a character), a previous specification (c("SA_spec", "X13") object) or a seasonal adjustment model (c("SA", "X13") object).')
+add_func("x13_pickmdl", "pickmdl", keyword = "r sesongjustering", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd",
+      name = "x13 with PICKMDL and partial concurrent possibilities",
+      descrip="x13 can be run as usual (automdl) or with a PICKMDL specification. The ARIMA model, outliers and filters can be identified at a certain date and then held fixed (with a new outlier-span).")
+add_func("x13_automdl", "pickmdl", keyword = "r sesongjustering", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd",
+    name = "x13 with PICKMDL and partial concurrent possibilities",
+    descrip = "x13 can be run as usual (automdl) or with a PICKMDL specification. The ARIMA model, outliers and filters can be identified at a certain date and then held fixed (with a new outlier-span).")
+add_func("x13_both", "pickmdl", keyword = "r sesongjustering", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_both.Rd",
+         name = "x13_spec and x13_pickmdl wrapped as a single function",
+         descrip = "Output is determined by the parameter: both_output.")
 
 
 # Analyse
@@ -80,6 +93,5 @@ add_func("quantile_weighted", "SSBtools", keyword = "r analyse vektberegning")
 # Usikkerhetsberegning
 add_func("CalibrateSSB", "CalibrateSSB", keyword = "r vektberegning usikkerhetberegning designbasert")
 add_func("PanelEstimation", "CalibrateSSB", keyword = "r vektberegning usikkerhetberegning designbasert")
-
 
 
