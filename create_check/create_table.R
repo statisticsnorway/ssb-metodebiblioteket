8ad6638544b061c29053f79d1aa3f9cbfab9d59c
Susie Jentoft <coo@ssb.no>
2023-07-14 13:43:53 +0200

    Removed github parameter.

--- create_check/create_table.R
+++ create_check/create_table.R
@@ -11,10 +11,10 @@ devtools::install_github("DiegoZardetto/ReGenesees")
 install.packages("GISSB")
 install.packages("GaussSupression")
 
-
 # RJDemetra litt vanskelig å installere pga avhengighet på java versjon
 # devtools::install_github("statisticsnorway/pickmdl")
 # install.packages("RJDemetra")
+
 
 
 #### Add functions ####
@@ -23,14 +23,14 @@ source("R/add_funcs.R")
 # Kontrollere funksjoner
 add_func('validator', 'validate', keyword = "r kontrollere logisk")
 add_func('confront', 'validate', keyword = "r kontrollere logisk")
-add_func('ThError', 'Kostra', keyword = "r kontrollere fordeling", github = T)
-add_func('Hb', 'Kostra', keyword = "r kontrollere fordeling", github = T)
-add_func('Quartile', 'Kostra', keyword = "r kontrollere fordeling", github = T)
-add_func('OutlierRegressionMicro', 'Kostra', keyword = "r kontrollere innflytelse", github = T)
-add_func('Rank2NumVar', 'Kostra', keyword = "r kontrollere fordeling", github = T)
-add_func("Diff2NumVar", "Kostra", keyword = "r kontrollere fordeling", github = T)
-add_func("AggrSml2NumVar", "Kostra", keyword = "r kontrollere fordeling", github = T)
-add_func("get_extremes", "struktuR", keyword = "r kontrollere innflytelse", github = T)
+add_func('ThError', 'Kostra', keyword = "r kontrollere fordeling")
+add_func('Hb', 'Kostra', keyword = "r kontrollere fordeling")
+add_func('Quartile', 'Kostra', keyword = "r kontrollere fordeling")
+add_func('OutlierRegressionMicro', 'Kostra', keyword = "r kontrollere innflytelse")
+add_func('Rank2NumVar', 'Kostra', keyword = "r kontrollere fordeling" )
+add_func("Diff2NumVar", "Kostra", keyword = "r kontrollere fordeling" )
+add_func("AggrSml2NumVar", "Kostra", keyword = "r kontrollere fordeling" )
+add_func("get_extremes", "struktuR", keyword = "r kontrollere innflytelse" )
 
 # Imputere
 add_func('impute_proxy', 'simputation', keyword = "r imputere donor")
@@ -46,9 +46,9 @@ add_func("model_aggregate", "SSBtools", keyword = "r strukturere 6.2")
 
 # Konfidensialitet
 add_func("PLSroundingPublish", "SmallCountRounding", keyword = "r konfidensialitet avrunding 6.4")
-add_func("ProtectKostra", "Kostra", keyword = "r konfidensialitet undertrykking 6.4", github = T)
-add_func("ProtectTableData", "easySdcTable", keyword = "r konfidensialitet undertrykking 6.4", github = T)
-add_func("SdcForetakPerson", "SdcForetakPerson", keyword = "r konfidensialitet undertrykking avrunding 6.4", github = T)
+add_func("ProtectKostra", "Kostra", keyword = "r konfidensialitet undertrykking 6.4" )
+add_func("ProtectTableData", "easySdcTable", keyword = "r konfidensialitet undertrykking 6.4" )
+add_func("SdcForetakPerson", "SdcForetakPerson", keyword = "r konfidensialitet undertrykking avrunding 6.4" )
 
 add_func("SuppressSmallCounts", "GaussSuppression", keyword = "r konfidensialitet undertrykking 6.4")
 add_func("SuppressDominantCells", "GaussSuppression", keyword = "r konfidensialitet undertrykking 6.4")
@@ -61,19 +61,19 @@ add_func("SuppressionFromDecimals", "GaussSuppression", keyword = "r konfidensia
 
 # Sesongjustering - missing html for functions in pickmdl. Can't install with Java so use manual
 
-add_func("x13", "RJDemetra", keyword = "r sesongjustering 6.1", url="https://rdrr.io/cran/RJDemetra/man/x13.html",
+add_func("x13", "RJDemetra", keyword = "r sesongjustering 6.1",
          name = "Seasonal Adjustment with X13-ARIMA",
          descrip = "Functions to estimate the seasonally adjusted series (sa) with the X13-ARIMA method. This is achieved by decomposing the time series (y) into the trend-cycle (t), the seasonal component (s) and the irregular component (i). The final seasonally adjusted series shall be free of seasonal and calendar-related movements. x13 returns a preformatted result while jx13 returns the Java objects resulting from the seasonal adjustment.")
-add_func("x13_spec", "RJDemetra", keyword = "r sesongjustering 6.1", url = "https://rdrr.io/cran/RJDemetra/man/x13_spec.html",
+add_func("x13_spec", "RJDemetra", keyword = "r sesongjustering 6.1", 
         name = "X-13ARIMA model specification, SA/X13",
         descrip = 'Function to create (and/or modify) a c("SA_spec", "X13") class object with the SA model specification for the X13 method. It can be done from a pre-defined "JDemetra+" model specification (a character), a previous specification (c("SA_spec", "X13") object) or a seasonal adjustment model (c("SA", "X13") object).')
-add_func("x13_pickmdl", "pickmdl", keyword = "r sesongjustering 6.1", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd",
+add_func("x13_pickmdl", "pickmdl", keyword = "r sesongjustering 6.1" , url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd",
       name = "x13 with PICKMDL and partial concurrent possibilities",
       descrip="x13 can be run as usual (automdl) or with a PICKMDL specification. The ARIMA model, outliers and filters can be identified at a certain date and then held fixed (with a new outlier-span).")
-add_func("x13_automdl", "pickmdl", keyword = "r sesongjustering 6.1", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd",
+add_func("x13_automdl", "pickmdl", keyword = "r sesongjustering 6.1", url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_pickmdl.Rd",
     name = "x13 with PICKMDL and partial concurrent possibilities",
     descrip = "x13 can be run as usual (automdl) or with a PICKMDL specification. The ARIMA model, outliers and filters can be identified at a certain date and then held fixed (with a new outlier-span).")
-add_func("x13_both", "pickmdl", keyword = "r sesongjustering 6.1", github = T, url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_both.Rd",
+add_func("x13_both", "pickmdl", keyword = "r sesongjustering 6.1", url="https://github.com/statisticsnorway/pickmdl/blob/main/man/x13_both.Rd",
          name = "x13_spec and x13_pickmdl wrapped as a single function",
          descrip = "Output is determined by the parameter: both_output.")
 
@@ -83,14 +83,13 @@ add_func("shortest_path_igraph", "GISSB", keyword = "r analyse nettverksanalyse 
 add_func("shortest_path_cppRouting", "GISSB", keyword = "r analyse nettverksanalyse romlig gis")
 
 # Indeksberegning
-add_func("CalcInd", "SSBpris", keyword = "r indeksberegning", github = T)
-add_func("CalcIndS2", "SSBpris", keyword = "r indeksberegning usikkerhetsberegning", github = T)
+add_func("CalcInd", "SSBpris", keyword = "r indeksberegning")
+add_func("CalcIndS2", "SSBpris", keyword = "r indeksberegning usikkerhetsberegning")
 
 # Vektberegning
-add_func("struktur_model", "struktuR", keyword = "r vektberegning modellbasert", github = T, 
+add_func("struktur_model", "struktuR", keyword = "r vektberegning modellbasert", 
          url = "https://statisticsnorway.github.io/struktuR/reference/struktur_model.html")
 add_func("quantile_weighted", "SSBtools", keyword = "r analyse vektberegning")
-
 
 # Usikkerhetsberegning
 add_func("CalibrateSSB", "CalibrateSSB", keyword = "r vektberegning usikkerhetberegning designbasert")
