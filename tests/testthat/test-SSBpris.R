076d0082216d8ffa4d267496a81240de7d3a0f27
Susie Jentoft <coo@ssb.no>
2023-07-14 13:27:50 +0200

    Copied tests from pacakges and added to.

---
+++ tests/testthat/test-SSBpris.R
@@ -0,0 +1,20 @@
+
+
+test_that("CalcInd returns correct value", {
+  data(priceData, package = "SSBpris")
+  ind <- CalcInd(data = priceData, baseVar = "b1", pVar = "p1", groupVar = "varenr", wVar = "weight", 
+                 consumVar = "coicop", type = "dutot")
+  expect_equal(as.numeric(ind[1]), 30.06709, tolerance = 1E-4)
+})
+
+
+
+test_that("CalcIndS2 returns correct values", {
+    data(priceData, package = "SSBpris")
+    ss <- CalcIndS2(data = priceData, baseVar = "b1", pVar = "p1", groupVar = "coicop", 
+                          type = "jevons")
+    expect_equal(length(ss), 3)
+    expect_equal(as.numeric(ss$s2[1]), 0.0019782, tolerance=1E-4)
+
+})
+
